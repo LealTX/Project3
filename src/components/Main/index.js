1ef787506241e68ffc22f8d117b5e13071f6b074
LealTX <jacob.leal32@gmail.com>
2019-01-23 14:19:40 -0600

    clean up

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -7,25 +7,10 @@ class MainGameContainer extends Component {
 		message: "Click Here to Log In!",
 	};
 
-	OnClickLogin = () => {
-		console.log("clicked")
-	};
-
 	render() {
 		return (
 			<div className="container-fluid mainCardContainer">
-				<div className="gameMessage text-center">
-				</div>
-				<div className="gameScores text-center">
-					<p>Score: {this.state.score} | Top Score: {this.state.topScore}</p>
-				</div>
-				<div className="container">
 
-					<div className="row">
-
-					</div>
-
-				</div>
 			</div>
 		);
 	}
