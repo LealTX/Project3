4e5de9058b47c8c2cfaadb7618de07e2a8222ffc
LealTX <jacob.leal32@gmail.com>
2019-01-31 20:00:41 -0600

    heroku push

--- client/src/components/LoginButton/index.js
+++ client/src/components/LoginButton/index.js
@@ -82,7 +82,7 @@ class LoginButton extends Component {
                         Party Vibes
                     </ul>
                     <ul className="right hide-on-med-and-down loginDisplay">
-                        <li><a href='https://secure-sierra-79189.herokuapp.com/login'><Icon left>person</Icon>Login to Spotify!</a></li>
+                        <li><a href='http://localhost:8888/login'><Icon left>person</Icon>Login to Spotify!</a></li>
                     </ul>
                 </div>
             </nav>
--- server.js
+++ server.js
@@ -139,9 +139,9 @@ app.get('/refresh_token', function(req, res) {
   });
 });
 
-app.get("/signin", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/login.html"));
-});
+// app.get("/signin", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/login.html"));
+// });
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
